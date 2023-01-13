f2d34b7ecfb70b5b7fb2bcd000d637083f444896
Shaghayegh <122172570+ShaghayeghHasani@users.noreply.github.com>
2023-01-13 18:25:37 +0330

    Update Educational status.py

--- Educational status.py
+++ Educational status.py
@@ -7,9 +7,9 @@ g3= float(input ("Your Literature score:"))
 average = (g1+g2+g3) / 3
 if 20>= average and average >=17 :
     result= "Great!"
-if average <= 17 and average >= 12:
+if average < 17 and average >= 12:
     result= "Normal"
-if average <= 12 and average >= 0:
+if average < 12 and average >= 0:
     result="Fail"
 
 print(name,Ln, " Your Average status is " , result, ".", "Would you like to see your average score?")
